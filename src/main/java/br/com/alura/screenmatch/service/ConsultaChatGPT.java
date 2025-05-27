7cc97b7b6478ace75a922acba89154cc2a6a99db
Rodrigo Soares <soaresjw27@gmail.com>
2025-05-27 10:08:14 -0300

    salvando arquivos com a classe serie

---
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,22 @@
+
+package br.com.alura.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+
+public class ConsultaChatGPT {
+    // API CHATGPT PARA TRADUCAO, REQUER CHAVE
+//    public static String obterTraducao(String texto) {
+//
+//        OpenAiService service = new OpenAiService("cole aqui sua chave da OpenAI");
+//
+//        CompletionRequest requisicao = CompletionRequest.builder()
+//       .model("gpt-3.5-turbo-instruct")
+//       .prompt("traduza para o português o texto: " + texto)
+//       .build();
+//
+//       var resposta = service.createCompletion(requisicao);
+//        return resposta.getChoices().get(0).getText();
+//    }
+}
